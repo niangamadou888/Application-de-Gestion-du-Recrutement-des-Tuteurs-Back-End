2ff3ace2a5ab977ca742a6337b361122682ed7a0
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2025-01-27 21:53:38 +0000

    fix CorsConfiguration

--- Back-End/src/main/java/com/project/backend/configuration/CorsConfiguration.java
+++ Back-End/src/main/java/com/project/backend/configuration/CorsConfiguration.java
@@ -1,10 +1,11 @@
 package com.project.backend.configuration;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configurable
+@Configuration
 public class CorsConfiguration {
 
     private static final String GET = "GET";
@@ -12,16 +13,19 @@ public class CorsConfiguration {
     private static final String DELETE = "DELETE";
     private static final String PUT = "PUT";
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods(GET,PUT,POST,DELETE)
                         .allowedHeaders("*")
                         .allowedOriginPatterns("*")
                         .allowCredentials(true);
+
             }
         };
     }
